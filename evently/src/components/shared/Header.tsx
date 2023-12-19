625b9f498f3fb6b6451f8c4e803396df42ec7b8c
Andry Ariadi <andryariadi23@gmail.com>
2023-12-19 21:37:57 +0700

    add env file

--- evently/src/components/shared/Header.tsx
+++ evently/src/components/shared/Header.tsx
@@ -1,7 +1,20 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Header() {
   return (
     <>
-      <h1>Header Component</h1>
+      <header className="w-full border-b">
+        <div className="wrapper flex items-center justify-between">
+          <Link href="/" className="w-36">
+            <Image src="/assets/images/evently.svg" alt="Evently" width={128} height={38} />
+          </Link>
+          <div className="flex w-32 justify-end gap-3 bg-orange-800">
+            <div>1</div>
+            <div>2</div>
+          </div>
+        </div>
+      </header>
     </>
   );
 }
