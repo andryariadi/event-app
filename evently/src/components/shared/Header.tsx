c1ab10b059b92ccbc450b837ed91e9e927ab8380
Andry Ariadi <andryariadi23@gmail.com>
2023-12-19 22:42:52 +0700

    add sign-in and sing-up page

--- evently/src/components/shared/Header.tsx
+++ evently/src/components/shared/Header.tsx
@@ -1,5 +1,7 @@
+import { SignedOut } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
+import { Button } from "../ui/button";
 
 export default function Header() {
   return (
@@ -9,9 +11,12 @@ export default function Header() {
           <Link href="/" className="w-36">
             <Image src="/assets/images/evently.svg" alt="Evently" width={128} height={38} />
           </Link>
-          <div className="flex w-32 justify-end gap-3 bg-orange-800">
-            <div>1</div>
-            <div>2</div>
+          <div className="flex w-32 justify-end gap-3">
+            <SignedOut>
+              <Button asChild className="rounded-full" size="lg">
+                <Link href="/sign-in">Login</Link>
+              </Button>
+            </SignedOut>
           </div>
         </div>
       </header>
