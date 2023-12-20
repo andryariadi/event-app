9371c83900e2cc36eb0ad9fcf3819d1de945b77d
Andry Ariadi <andryariadi23@gmail.com>
2023-12-20 21:12:08 +0700

    update footer

--- evently/src/components/shared/Footer.tsx
+++ evently/src/components/shared/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <>
       <footer className="border-t">
         <div className="wrapper flex flex-center flex-between flex-col sm:flex-row text-center gap-4 p-5">
-          <Image src="/assets/images/evently.svg" alt="Evently" width={100} height={38} />
+          <Image src="/assets/images/evently.svg" alt="evently" width={100} height={38} />
           <p className="text-rose-600 font-medium">AndryAriadi © 2023 Evently App. All rights reserved.</p>
         </div>
       </footer>
