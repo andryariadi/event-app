9b66240500a439949f980ba23067467081d8f1f4
Andry Ariadi <andryariadi23@gmail.com>
2023-12-21 06:27:07 +0700

    install react datepicker

--- evently/src/components/shared/EventForm.tsx
+++ evently/src/components/shared/EventForm.tsx
@@ -12,6 +12,7 @@ import Dropdown from "./Dropdown";
 import { Textarea } from "@/components/ui/textarea";
 import { FileUploader } from "./FileUploader";
 import { useState } from "react";
+import Image from "next/image";
 
 type EventFormProps = {
   userId: string;
@@ -83,6 +84,40 @@ export default function EventForm({ userId, type }: EventFormProps) {
                 <FormItem className="w-full">
                   <FormControl className="h-64">
                     <FileUploader onFieldChange={field.onChange} imageUrl={field.value} setFiles={setFiles} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="flex flex-col md:flex-row gap-5">
+            <FormField
+              control={form.control}
+              name="location"
+              render={({ field }) => (
+                <FormItem className="w-full">
+                  <FormControl>
+                    <div className="flex-center h-[54px] w-full overflow-hidden rounded-full bg-grey-50 px-4 py-2">
+                      <Image src="/assets/icons/location-grey.svg" alt="location" width={24} height={24} />
+                      <Input placeholder="Event location or Online" {...field} className="input-field" />
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="flex flex-col md:flex-row gap-5">
+            <FormField
+              control={form.control}
+              name="startDateTime"
+              render={({ field }) => (
+                <FormItem className="w-full">
+                  <FormControl>
+                    <div className="flex-center h-[54px] w-full overflow-hidden rounded-full bg-grey-50 px-4 py-2">
+                      <Image src="/assets/icons/calendar.svg" alt="calendar" width={24} height={24} className="filter-grey" />
+                      <p className="ml-3 whitespace-nowrap text-gray-600">Start Date:</p>
+                    </div>
                   </FormControl>
                   <FormMessage />
                 </FormItem>
