bf2d39f3ac762ba0205980f3948ed68563699bad
Andry Ariadi <andryariadi23@gmail.com>
2023-12-20 21:45:57 +0700

    add create event page

---
+++ evently/src/app/(root)/events/create/page.tsx
@@ -0,0 +1,12 @@
+export default function CreateEventPage() {
+  return (
+    <>
+      <section className="bg-primary-50 bg-dotted-pattern bg-center bg-cover py-5 md:py-10">
+        <h3 className="wrapper h3-bold text-center sm:text-left">Create Event</h3>
+      </section>
+      <div className="wrapper my-8">
+        <h1>Create Event</h1>
+      </div>
+    </>
+  );
+}
