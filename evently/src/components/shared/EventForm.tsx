8945190a95c3ebf7e133af10c05894abb250b6de
Andry Ariadi <andryariadi23@gmail.com>
2023-12-21 06:50:45 +0700

    setup datepicker

--- evently/src/components/shared/EventForm.tsx
+++ evently/src/components/shared/EventForm.tsx
@@ -13,6 +13,8 @@ import { Textarea } from "@/components/ui/textarea";
 import { FileUploader } from "./FileUploader";
 import { useState } from "react";
 import Image from "next/image";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 type EventFormProps = {
   userId: string;
@@ -117,6 +119,23 @@ export default function EventForm({ userId, type }: EventFormProps) {
                     <div className="flex-center h-[54px] w-full overflow-hidden rounded-full bg-grey-50 px-4 py-2">
                       <Image src="/assets/icons/calendar.svg" alt="calendar" width={24} height={24} className="filter-grey" />
                       <p className="ml-3 whitespace-nowrap text-gray-600">Start Date:</p>
+                      <DatePicker selected={field.value} onChange={(date: Date) => field.onChange(date)} showTimeSelect timeInputLabel="Time:" dateFormat="MM/dd/yyyy h:mm aa" wrapperClassName="datePicker" />
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="endDateTime"
+              render={({ field }) => (
+                <FormItem className="w-full">
+                  <FormControl>
+                    <div className="flex-center h-[54px] w-full overflow-hidden rounded-full bg-grey-50 px-4 py-2">
+                      <Image src="/assets/icons/calendar.svg" alt="calendar" width={24} height={24} className="filter-grey" />
+                      <p className="ml-3 whitespace-nowrap text-gray-600">End Date:</p>
+                      <DatePicker selected={field.value} onChange={(date: Date) => field.onChange(date)} showTimeSelect timeInputLabel="Time:" dateFormat="MM/dd/yyyy h:mm aa" wrapperClassName="datePicker" />
                     </div>
                   </FormControl>
                   <FormMessage />
