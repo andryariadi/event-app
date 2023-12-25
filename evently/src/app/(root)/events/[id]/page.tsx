faa71eb3e3c23b4ae605c73032a648347bd47525
Andry Ariadi <andryariadi23@gmail.com>
2023-12-25 07:28:30 +0700

    update styling detail event page

--- evently/src/app/(root)/events/[id]/page.tsx
+++ evently/src/app/(root)/events/[id]/page.tsx
@@ -11,46 +11,46 @@ export default async function EventDetailPage({ params: { id } }: SearchParamPro
   return (
     <>
       <section className="flex justify-center bg-primary-50 bg-dotted-pattern bg-center bg-contain py-5 md:py-10">
-        <div className="bg-rose-600 grid grid-cols-1 md:grid-cols-2 2xl:max-w-7xl">
-          <Image src={event.imageUrl} alt="event" width={1000} height={1000} className="h-full min-h[300px] object-cover object-center" />
-        </div>
-        <div className="flex flex-col w-full gap-8 p-5 md:p-10">
-          <div className="flex flex-col gap-6">
-            <h2 className="h2-bold">{event.title}</h2>
-            <div className="flex flex-col gap-3 sm:flex-row sm:items-center">
-              <div className="flex gap-3">
-                <p className="p-bold-20 rounded-full bg-green-500/10 py-2 px-5 text-green-700">{event.isFree ? "Free" : `$${event.price}`}</p>
-                <p className="p-medium-16 rounded-full bg-grey-500/10 text-grey-500 px-4 py-2.5">{event.category.name}</p>
-              </div>
-              <p className="p-medium-18 ml-2 mt-2 sm:mt-0">
-                by{" "}
-                <span className="text-primary-500">
-                  {event.organizer.firstName} {event.organizer.lastName}
-                </span>
-              </p>
-            </div>
-          </div>
-          <div className="flex flex-col gap-5">
-            <div className="flex flex-col md:flex-row gap-2 md:gap-3">
-              <Image src="/assets/icons/calendar.svg" alt="calendar" width={32} height={32} />
-              <div className="flex flex-wrap items-center gap-x-1 p-medium-16 lg:p-regular-20">
-                <p>
-                  {formatDateTime(event.startDateTime).dateOnly} - {formatDateTime(event.startDateTime).timeOnly}
-                </p>
-                <p>
-                  {formatDateTime(event.endDateTime).dateOnly} - {formatDateTime(event.endDateTime).timeOnly}
+        <div className="grid grid-cols-1 md:grid-cols-2 2xl:max-w-7xl md:flex-center gap-x-5">
+          <Image src={event.imageUrl} alt="event" width={500} height={500} className="h-full min-h[300px] object-cover object-center" />
+          <div className="flex flex-col w-full gap-8 p-5 md:p-10">
+            <div className="flex flex-col gap-6">
+              <h2 className="h2-bold">{event.title}</h2>
+              <div className="flex flex-col gap-3 sm:flex-row sm:items-center">
+                <div className="flex gap-3">
+                  <p className="p-bold-20 rounded-full bg-green-500/10 py-2 px-5 text-green-700">{event.isFree ? "Free" : `$${event.price}`}</p>
+                  <p className="p-medium-16 rounded-full bg-grey-500/10 text-grey-500 px-4 py-2.5">{event.category.name}</p>
+                </div>
+                <p className="p-medium-18 ml-2 mt-2 sm:mt-0">
+                  by{" "}
+                  <span className="text-primary-500">
+                    {event.organizer.firstName} {event.organizer.lastName}
+                  </span>
                 </p>
               </div>
             </div>
-            <div className="flex items-center gap-3 p-regular-20">
-              <Image src="/assets/icons/location.svg" alt="location" width={32} height={32} className="" />
-              <p className="p-medium-16 lg:p-regular-20">{event.location}</p>
+            <div className="flex flex-col gap-5">
+              <div className="flex gap-2 md:gap-3">
+                <Image src="/assets/icons/calendar.svg" alt="calendar" width={32} height={32} />
+                <div className="flex flex-wrap items-center gap-y-1 p-medium-16 lg:p-regular-20">
+                  <p>
+                    {formatDateTime(event.startDateTime).dateOnly} - {formatDateTime(event.startDateTime).timeOnly}
+                  </p>
+                  <p>
+                    {formatDateTime(event.endDateTime).dateOnly} - {formatDateTime(event.endDateTime).timeOnly}
+                  </p>
+                </div>
+              </div>
+              <div className="flex items-center gap-3 p-regular-20">
+                <Image src="/assets/icons/location.svg" alt="location" width={32} height={32} className="" />
+                <p className="p-medium-16 lg:p-regular-20">{event.location}</p>
+              </div>
             </div>
-          </div>
-          <div className="flex flex-col gap-2">
-            <p className="p-bold-20 text-grey-600">What You Will Learn:</p>
-            <p className="p-medium-16 lg:p-regular-18">{event.description}</p>
-            <p className="p-medium-16 lg:p-regular-18 truncate text-primary-500 cursor-pointer">{event.url.length > 30 ? `${event.url.substring(0, 30)}...` : event.url}</p>
+            <div className="flex flex-col gap-2">
+              <p className="p-bold-20 text-grey-600">What You Will Learn:</p>
+              <p className="p-medium-16 lg:p-regular-18">{event.description}</p>
+              <p className="p-medium-16 lg:p-regular-18 truncate text-primary-500 cursor-pointer">{event.url.length > 30 ? `${event.url.substring(0, 30)}...` : event.url}</p>
+            </div>
           </div>
         </div>
       </section>
