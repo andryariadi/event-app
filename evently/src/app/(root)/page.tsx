5599c2b9bf17854e8813752f0d4de900cc1d93a5
Andry Ariadi <andryariadi23@gmail.com>
2023-12-25 14:16:30 +0700

    fetch all events

--- evently/src/app/(root)/page.tsx
+++ evently/src/app/(root)/page.tsx
@@ -1,9 +1,19 @@
 import Collection from "@/components/shared/Collection";
 import { Button } from "@/components/ui/button";
+import { getAllEvents } from "@/lib/actions/event.action";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const events = await getAllEvents({
+    query: "",
+    category: "",
+    limit: 6,
+    page: 1,
+  });
+
+  console.log(events, "<-----dihomepage");
+
   return (
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
@@ -31,7 +41,7 @@ export default function Home() {
           <div>Search</div>
           <div>CategoryFilter</div>
         </div>
-        <Collection data={[]} emptyTitle="Sorry, we could not find any events" emptyStateSubtext="Come back latter" collectionType="All_Events" limit={6} page={1} totalPages={2} />
+        <Collection data={events?.data} emptyTitle="Sorry, we could not find any events" emptyStateSubtext="Come back latter" collectionType="All_Events" limit={6} page={1} totalPages={2} />
       </section>
     </>
   );
--- evently/src/components/shared/Collection.tsx
+++ evently/src/components/shared/Collection.tsx
@@ -12,7 +12,14 @@ type CollectionProps = {
 export default function Collection({ data, emptyTitle, emptyStateSubtext, collectionType, limit, page, totalPages = 0, urlParamName }: CollectionProps) {
   return (
     <>
-      <h1>collection component</h1>
+      {data.length > 0 ? (
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 w-full gap-5 xl:gap-10"></div>
+      ) : (
+        <div className="wrapper flex-center flex-col gap-3 w-full min-h-[200px] py-28 rounded-[14px] bg-grey-50 text-center">
+          <h3 className="p-bold-20 md:h5-bold">{emptyTitle}</h3>
+          <p className="p-regular-14">{emptyStateSubtext}</p>
+        </div>
+      )}
     </>
   );
 }
