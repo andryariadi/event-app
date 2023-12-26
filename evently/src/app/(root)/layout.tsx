edbb6b881e565ebf792b2504f18a865f94ff4870
Andry Ariadi <andryariadi23@gmail.com>
2023-12-26 07:13:37 +0700

    update styling header

--- evently/src/app/(root)/layout.tsx
+++ evently/src/app/(root)/layout.tsx
@@ -3,9 +3,9 @@ import Header from "@/components/shared/Header";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex flex-col h-screen">
+    <div className="flex flex-col h-screen bg-primary-50 bg-dotted-pattern bg-contain">
       <Header />
-      <main className="flex-1">{children}</main>
+      <main className="flex-1 mt-[86px]">{children}</main>
       <Footer />
     </div>
   );
--- evently/src/components/shared/Header.tsx
+++ evently/src/components/shared/Header.tsx
@@ -1,14 +1,31 @@
+"use client";
+
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "../ui/button";
 import NavItems from "./NavItems";
 import MobileNav from "./MobileNav";
+import { useEffect, useState } from "react";
 
 export default function Header() {
+  const [changeColor, setChangeColor] = useState(false);
+
+  const changeBackgrounColor = () => {
+    if (window.scrollY > 10) {
+      setChangeColor(true);
+    } else {
+      setChangeColor(false);
+    }
+  };
+
+  useEffect(() => {
+    changeBackgrounColor();
+    window.addEventListener("scroll", changeBackgrounColor);
+  });
   return (
     <>
-      <header className="w-full border-b">
+      <header className={`w-full border-b fixed z-10 ${changeColor ? "bg-gradient-to-br from-white to-sky-100" : null} transition-all duration-300`}>
         <div className="wrapper flex items-center justify-between">
           <Link href="/" className="w-36">
             <Image src="/assets/images/evently.svg" alt="Evently" width={128} height={38} />
