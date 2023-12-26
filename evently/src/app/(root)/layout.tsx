df160dc4760ba49dc29bb6d1b557ee9f5cb6afe0
Andry Ariadi <andryariadi23@gmail.com>
2023-12-26 13:57:22 +0700

    add update event action

--- evently/src/app/(root)/layout.tsx
+++ evently/src/app/(root)/layout.tsx
@@ -3,7 +3,7 @@ import Header from "@/components/shared/Header";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex flex-col h-screen bg-primary-50 bg-dotted-pattern bg-contain">
+    <div className="flex flex-col h-screen bg-transparent">
       <Header />
       <main className="flex-1 mt-[86px]">{children}</main>
       <Footer />
