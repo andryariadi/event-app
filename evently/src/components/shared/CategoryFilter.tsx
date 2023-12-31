be86ea7ebcf37518dafd3447ed9663bff70a9189
Andry Ariadi <andryariadi23@gmail.com>
2024-01-01 06:18:47 +0700

    setup category filter

--- evently/src/components/shared/CategoryFilter.tsx
+++ evently/src/components/shared/CategoryFilter.tsx
@@ -1,7 +1,67 @@
+"use client";
+
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { getAllCategories } from "@/lib/actions/category.action";
+import { ICategory } from "@/lib/database/models/category.model";
+import { formUrlQuery, removeKeysFromQuery } from "@/lib/utils";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
 export default function CategoryFilter() {
+  const [categories, setCategories] = useState<ICategory[]>([]);
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  useEffect(() => {
+    const getCategories = async () => {
+      const categoryList = await getAllCategories();
+
+      categoryList && setCategories(categoryList as ICategory[]);
+    };
+
+    getCategories();
+  }, []);
+
+  //   useEffect(() => {
+  //     const delayBounceFn = setTimeout(() => {
+  //       let newUrl = "";
+  //       if (categories) {
+  //         newUrl = formUrlQuery({ params: searchParams.toString(), key: "categories", value: categories });
+  //       } else {
+  //         newUrl = removeKeysFromQuery({ params: searchParams.toString(), keysToRemove: ["categories"] });
+  //       }
+  //       router.push(newUrl, { scroll: false });
+  //     }, 300);
+  //     return () => clearTimeout(delayBounceFn);
+  //   }, [categories, searchParams, router]);
+
+  const onSelectCategory = (category: string) => {
+    let newUrl = "";
+    if (category && category !== "All") {
+      newUrl = formUrlQuery({ params: searchParams.toString(), key: "category", value: category });
+    } else {
+      newUrl = removeKeysFromQuery({ params: searchParams.toString(), keysToRemove: ["category"] });
+    }
+    router.push(newUrl, { scroll: false });
+  };
+
   return (
     <>
-      <h1>Filter</h1>
+      <Select onValueChange={(value: string) => onSelectCategory(value)}>
+        <SelectTrigger className="select-field">
+          <SelectValue placeholder="Category" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="All" className="select-item p-regular-14">
+            All
+          </SelectItem>
+          {categories.map((category) => (
+            <SelectItem key={category._id} value={category.name} className="select-item p-regular-14">
+              {category.name}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
     </>
   );
 }
